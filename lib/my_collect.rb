42c1d781f8afd00e1c3065718cffe724e48bacde
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-19 19:21:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,13 +8,13 @@ def my_collect(arr)
   # look at lambda or proc to solve this problem
   # because you have to pass the content of the modified block back to my_collect method
   if arr[0].length < 2
-  n_str= arr.join(",")
-  upp_str= n_str.upcase
-  n_arr= upp_str.split(",")
-  n_arr
-elsif arr[0].length == 2
-  arr.map do |elem|
-    elem.split(" ").first
+    n_str= arr.join(",")
+    upp_str= n_str.upcase
+    n_arr= upp_str.split(",")
+    n_arr
+  elsif arr[0].length == 2
+    arr.map do |elem|
+      elem.split(" ").first
   end
   else
   arr.length
