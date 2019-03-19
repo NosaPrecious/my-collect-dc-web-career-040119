825993240ba16e73c5966ac628e4831f9d726138
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-19 19:05:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,13 +7,16 @@ def my_collect(arr)
   end
   # look at lambda or proc to solve this problem
   # because you have to pass the content of the modified block back to my_collect method
+  if arr.length < 2
   n_str= arr.join(",")
   upp_str= n_str.upcase
   n_arr= upp_str.split(",")
   n_arr
+else
   arr.map do |elem|
     elem.split(" ").first
   end
+end
   else
     "not called by a block!"
   end
