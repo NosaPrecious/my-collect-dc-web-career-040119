eca48d646f236d0a8ad94a4a64bbe72e3c17e366
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-19 18:57:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
-
+def my_collect(arr)
+  if block_given?
+    count= 0
+    while count < arr.length
+    yield arr[count]
+    count+= 1
+  end
+  # look at lambda or proc to solve this problem
+  # because you have to pass the content of the modified block back to my_collect method
+  n_str= arr.join(",")
+  upp_str= n_str.upcase
+  n_arr= upp_str.split(",")
+  n_arr
+  
+  else
+    "not called by a block!"
+  end
+end
 
